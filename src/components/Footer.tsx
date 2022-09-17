fd165badcefde718746678ae1e244d4eaa54df4d
PathonScript <pathonscript@gmail.com>
2022-09-17 09:56:45 +0700

    feat: footer contents

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,19 @@ import styles from "../styles/Home.module.scss";
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <div></div>
+      <div>
+        <div>
+          <h4>449/183 Soi Suwinthawong 11, Suwinthawong Rd.</h4>
+          <h4>Sansab, Minburi , Bangkok 10510 Thailand</h4>
+        </div>
+      </div>
+      <div>
+        <div>
+          <p>Tel. : +66(0)2989-4663-4 </p>
+          <p>Mobile : +66(0)95254-9720 (Mr.Nutthawut)</p>
+          <p>Fax : +66(0)2989-4665</p>
+        </div>
+      </div>
     </footer>
   );
 }
