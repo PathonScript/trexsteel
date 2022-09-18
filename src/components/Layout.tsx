e0e70a723e68693e3772d2391d18b06762afc949
PathonScript <pathonscript@gmail.com>
2022-09-18 09:46:57 +0700

    feat: dropdown component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import Link from "next/link";
+
 import BigBar from "./BigBar";
 import DropDown from "./DropDown";
-
 import useWindowDimensions from "./hooks/useWindowDimensions";
+
+import mobile from "../styles/Mobile.module.scss";
 
 function Layout({ children }: React.PropsWithChildren) {
   const { height, width } = useWindowDimensions();
@@ -17,11 +20,30 @@ function Layout({ children }: React.PropsWithChildren) {
   } else {
     return (
       <>
-        <DropDown>Hello World</DropDown>
+        <DropDown>
+          <DropDownMenu />
+        </DropDown>
         <main>{children}</main>
       </>
     );
   }
+}
+
+interface DropDown {
+  link: string;
+  children: ReactNode;
+}
+
+function DropDownMenu() {
+  function DropDownItem(props: DropDown) {
+    return (
+      <Link href={props.link}>
+        <a>{props.children}</a>
+      </Link>
+    );
+  }
+
+  return <DropDownItem link="/">Home</DropDownItem>;
 }
 
 export default Layout;
